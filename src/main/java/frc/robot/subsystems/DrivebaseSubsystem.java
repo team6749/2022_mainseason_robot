6c113209ad3d471cce2c24713e6d55cf323a3344
ERA Robotics <35408444+ERArobotics@users.noreply.github.com>
2022-01-16 14:01:58 -0600

    it no work so fix

--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -1,22 +1,19 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 
 public class DrivebaseSubsystem extends SubsystemBase{
 
-
+    TalonFX frontLeft = new TalonFX(Constants.frontLeftMotor);
     
-public DrivebaseSubsystem() {}
+    public DrivebaseSubsystem() {
+        
+    }
 
-@Override
-public void periodic() {
-  // This method will be called once per scheduler run
-}
 
-@Override
-public void simulationPeriodic() {
-  // This method will be called once per scheduler run during simulation
-}
 
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,14 +6,4 @@ public class ShooterSubsystem extends SubsystemBase {
 
 
 public ShooterSubsystem() {}
-
-@Override
-public void periodic() {
-  // This method will be called once per scheduler run
-}
-
-@Override
-public void simulationPeriodic() {
-  // This method will be called once per scheduler run during simulation
-}
 }
