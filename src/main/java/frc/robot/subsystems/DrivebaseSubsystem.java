3b2e6d4528b104e3bd5e52272170018052ba216a
Laptop 0 <35408444+ERArobotics@users.noreply.github.com>
2022-01-16 14:13:24 -0600

    periodic things

--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -13,10 +13,17 @@ public class DrivebaseSubsystem extends SubsystemBase{
     TalonFX backLeft = new TalonFX(Constants.blMotor);
     TalonFX backRight = new TalonFX(Constants.brMotor);
     
-    public DrivebaseSubsystem() {
-        
-    }
+    public DrivebaseSubsystem() {}
 
+    @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
 
 
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,4 +6,15 @@ public class ShooterSubsystem extends SubsystemBase {
 
 
 public ShooterSubsystem() {}
+
+
+@Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
 }
