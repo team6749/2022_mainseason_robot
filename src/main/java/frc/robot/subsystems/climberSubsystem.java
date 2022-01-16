a1f94dd3f3d1c9b88bf998f81eb0738fe2be3ea9
ERA Robotics <35408444+ERArobotics@users.noreply.github.com>
2022-01-16 13:48:14 -0600

    flow

--- src/main/java/frc/robot/subsystems/climberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climberSubsystem.java
@@ -2,8 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class climberSubsystem extends SubsystemBase  {
+public class ClimberSubsystem extends SubsystemBase  {
     
 
-public climberSubsystem() {}
+public ClimberSubsystem() {}
+
 }
