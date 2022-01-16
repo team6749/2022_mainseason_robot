f7b2218f9677fae624b7b9f52b88fdba385e765d
Laptop 0 <35408444+ERArobotics@users.noreply.github.com>
2022-01-16 13:50:13 -0600

    sus

--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -3,11 +3,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
-public class driveSubsystem extends SubsystemBase{
+public class DrivebaseSubsystem extends SubsystemBase{
 
 
     
-public driveSubsystem() {}
+public DrivebaseSubsystem() {}
 
 
 
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class shootingSubsystem extends SubsystemBase {
+public class ShooterSubsystem extends SubsystemBase {
 
 
-public shootingSubsystem() {}
+public ShooterSubsystem() {}
 }
