fc4a71eaf29980f30629e545ae9304ea562161e6
Laptop 0 <35408444+ERArobotics@users.noreply.github.com>
2022-01-20 18:42:06 -0600

    motorcontroller groups

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,9 +20,10 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  XboxController controller = new XboxController(0);
-  
-  // The robot's subsystems and commands are defined here...
+ 
+  Joystick joystick = new Joystick(0);
+   
+    // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -1,34 +1,48 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj.drive.*;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 
 
 
 public class DrivebaseSubsystem extends SubsystemBase{
 
-    TalonFX frontLeft = new TalonFX(Constants.flMotor);
-    TalonFX frontRight = new TalonFX(Constants.frMotor);
-    TalonFX backLeft = new TalonFX(Constants.blMotor);
-    TalonFX backRight = new TalonFX(Constants.brMotor);
+    private final WPI_TalonFX frontLeft = new WPI_TalonFX(Constants.flMotor);
+    private final WPI_TalonFX frontRight = new WPI_TalonFX(Constants.frMotor);
+    private final WPI_TalonFX backLeft = new WPI_TalonFX(Constants.blMotor);
+    private final WPI_TalonFX backRight = new WPI_TalonFX(Constants.brMotor);
+    
+    MotorControllerGroup left = new MotorControllerGroup(frontLeft, backLeft);
+    MotorControllerGroup right = new MotorControllerGroup(frontRight, backRight);
+    
+    DifferentialDrive myDrive = new DifferentialDrive(left, right);
     
     
+
     public DrivebaseSubsystem() {
-        
-    }
-    
-    public void DriveWithController(){
-    
+      
     }
 
+    public void arcadeDrive(double speed, double rotation){
+      
+      myDrive.arcadeDrive(speed, rotation);
+    }
 
     @Override
   public void periodic() {
+    
+    
+    }
     // This method will be called once per scheduler run
-  }
+  
 
   @Override
   public void simulationPeriodic() {
