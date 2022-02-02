6e54bbf658b4fc888d51fd75754107c6cb9bca4b
Laptop 0 <35408444+ERArobotics@users.noreply.github.com>
2022-02-01 19:05:59 -0600

    sus systems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ClimberSubsystem;
+import frc.robot.subsystems.DrivebaseSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -23,7 +27,11 @@ public class RobotContainer {
    
     // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
+  private final ClimberSubsystem _ClimberSubsystem = new ClimberSubsystem();
+  private final DrivebaseSubsystem _DrivebaseSubsystem = new DrivebaseSubsystem();
+  private final IntakeSubsystem _IntakeSubsystem = new IntakeSubsystem();
+  private final ShooterSubsystem _ShooterSubsystem = new ShooterSubsystem();
+  //commands - usually not put here 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
