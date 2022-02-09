48f85bbdb78fa6060fc915b52619df43ba346f50
ERA Robotics <35408444+ERArobotics@users.noreply.github.com>
2022-02-08 19:09:01 -0600

    removed examples

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.DrivebaseSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -26,13 +24,11 @@ public class RobotContainer {
 
    
     // The robot's subsystems and commands are defined here
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final ClimberSubsystem _ClimberSubsystem = new ClimberSubsystem();
   private final DrivebaseSubsystem _DrivebaseSubsystem = new DrivebaseSubsystem();
   private final IntakeSubsystem _IntakeSubsystem = new IntakeSubsystem();
   private final ShooterSubsystem _ShooterSubsystem = new ShooterSubsystem();
-  //commands - usually not put here 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  //commands - usually not put here
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -55,6 +51,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
