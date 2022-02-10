ebfbc2d4996447cbdff7a4950055d0fa05dc4536
ERA Robotics <35408444+ERArobotics@users.noreply.github.com>
2022-02-10 17:33:24 -0600

    removed garbage

--- src/main/java/frc/robot/commands/AutoIntakeBalls.java
+++ src/main/java/frc/robot/commands/AutoIntakeBalls.java
@@ -26,7 +26,7 @@ public class AutoIntakeBalls extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(IntakeSubsystem.)
+    
   }
 
   // Called once the command ends or is interrupted.
