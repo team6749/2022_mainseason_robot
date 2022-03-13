fbeaf7ed1d6b5b694d5476fc5795b1af687f1571
Laptop 0 <35408444+ERArobotics@users.noreply.github.com>
2022-03-13 16:39:19 -0500

    fixed main intake and shgooter

--- src/main/java/frc/robot/commands/DriveForwardAutonomously.java
+++ src/main/java/frc/robot/commands/DriveForwardAutonomously.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivebaseSubsystem;
