0e00cee0fcfabaeee132828c5d07496bdcecbffb
ERA Robotics <35408444+ERArobotics@users.noreply.github.com>
2022-03-15 17:59:38 -0500

    merge pneumatics code + control

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ClimberControl;
 import frc.robot.commands.DriveForwardAutonomously;
+import frc.robot.commands.SetSmallArmState;
 import frc.robot.enums.ClimberDirection;
+import frc.robot.enums.SmallArmState;
 import frc.robot.commands.AutoIntakeBalls;
 import frc.robot.commands.ShootAllBalls;
 import frc.robot.commands.driveWithJoystick;
@@ -38,6 +40,10 @@ public class RobotContainer {
   final JoystickButton climberUpButton = new JoystickButton(leftJoystick, 3); //climber up
   final JoystickButton climberDownButton = new JoystickButton(rightJoystick, 3); //climber down
   final JoystickButton shootBallsButton = new JoystickButton(rightJoystick, 2); //shoot ballz
+
+  final JoystickButton smallArmsForward = new JoystickButton(leftJoystick, 5); 
+  final JoystickButton smallArmsBackward = new JoystickButton(leftJoystick, 4);
+  final JoystickButton smallArmsOff = new JoystickButton(leftJoystick, 2);
   
   // final JoystickButton l3 = new JoystickButton(leftJoystick, 3);
   
@@ -79,6 +85,10 @@ public class RobotContainer {
     climberUpButton.whileHeld(new ClimberControl(_ClimberSubsystem, 0.1, ClimberDirection.UP));
     climberDownButton.whileHeld(new ClimberControl(_ClimberSubsystem, 0.1, ClimberDirection.DOWN));
     shootBallsButton.whenPressed(new ShootAllBalls(_ShooterSubsystem, _IntakeSubsystem));
+
+    smallArmsForward.whenPressed(new SetSmallArmState(_ClimberSubsystem, SmallArmState.FORWARD));
+    smallArmsBackward.whenPressed(new SetSmallArmState(_ClimberSubsystem, SmallArmState.BACKWARD));
+    smallArmsOff.whenPressed(new SetSmallArmState(_ClimberSubsystem, SmallArmState.OFF));
     
     //controler code bindings
 
